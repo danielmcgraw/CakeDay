3c524f720ac2487e29a33cc96f4d37ae58411f13
Daniel McGraw <dan.j.mcgraw@gmail.com>
2012-01-01 15:48:59 -0700

    Playing with js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,27 @@
 
 */
 
+$("#getRedditUNameForm").submit(function(){
+	var uname = $("#uname").val();
+		if (uname.length == 0) {
+			$("#uname").focus();
+		} else {
+			//$("#result").html(ajax_load);
+			$.getJSON(
+				"http://www.reddit.com/user/" + uname + "/about.json",
+//				"q="+q,
+				{uname: uname},
+				function(json) {
+					var result = "<p>Data \"<strong>" + json.responseData.data + "</strong>\"</p>";
+					$("#result").html(result);
+				}
+			);
+		}
+		return false;
+	});
+
+
+
 $(document).ready(function() {
 	$.ajax({
 	  url: "http://www.reddit.com/user/man_in_the_mirra/about.json",
