550c8bb0087eaa699371044ede9024ea38ed3b56
Daniel McGraw <dan.j.mcgraw@gmail.com>
2012-01-01 15:07:54 -0700

    Add a document on ready fuction and alert test function

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@
 
 */
 
+$(document).ready(function() {
+	$.ajax({
+	  url: http://www.reddit.com/user/man_in_the_mirra/about.json,
+	  success: alertJson(data),
+	  dataType: json
+	});
+});
 
-
-
-
-
-
+function alertJson(data) {
+	alert(data)
+}
