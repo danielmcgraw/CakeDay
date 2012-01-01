7ce52d9abe2987a0b92d8aadc54640c3c0f0bb59
Daniel McGraw <dan.j.mcgraw@gmail.com>
2012-01-01 15:21:08 -0700

    Fix up doc ready call and test

--- js/script.js
+++ js/script.js
@@ -4,12 +4,8 @@
 
 $(document).ready(function() {
 	$.ajax({
-	  url: http://www.reddit.com/user/man_in_the_mirra/about.json,
-	  success: alertJson(data),
-	  dataType: json
+	  url: "http://www.reddit.com/user/man_in_the_mirra/about.json",
+	  success: alert(data),
+	  dataType: "json"
 	});
 });
-
-function alertJson(data) {
-	alert(data)
-}
