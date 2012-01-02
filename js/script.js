e1d7d23a7c6250e3fbd51dc7fc681ffa4c36e2fe
Daniel McGraw <dan.j.mcgraw@gmail.com>
2012-01-02 15:29:07 -0700

    Unix time to local time

--- js/script.js
+++ js/script.js
@@ -15,9 +15,31 @@ $("#getRedditUNameForm").submit(function(){
 				url: "http://www.reddit.com/user/" + uname + "/about.json?jsonp=?",
 				dataType: "jsonp",
 				success: function(response) {
-					$("#result").html("<p>Data \"<strong>" + response.data.created_utc + "</strong>\"</p>");
+					$("#result").html("<p>Data \"<strong>" + formatTime(response.data.created_utc) + "</strong>\"</p>");
 				}
 			});
 		}
 	return false;
 });
+
+
+function formatTime(unixTimestamp) {
+    var dt = new Date(unixTimestamp * 1000);
+
+    var hours = dt.getHours();
+    var minutes = dt.getMinutes();
+    var seconds = dt.getSeconds();
+
+    // the above dt.get...() functions return a single digit
+    // so I prepend the zero here when needed
+    if (hours < 10) 
+     hours = '0' + hours;
+
+    if (minutes < 10) 
+     minutes = '0' + minutes;
+
+    if (seconds < 10) 
+     seconds = '0' + seconds;
+
+    return hours + ":" + minutes + ":" + seconds;
+}
