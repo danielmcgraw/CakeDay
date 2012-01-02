bd968a113da831eb9f08832ff727502c84da4b6e
Daniel McGraw <dan.j.mcgraw@gmail.com>
2012-01-02 15:34:09 -0700

    Datetime conversion

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ $("#getRedditUNameForm").submit(function(){
 				url: "http://www.reddit.com/user/" + uname + "/about.json?jsonp=?",
 				dataType: "jsonp",
 				success: function(response) {
-					$("#result").html("<p>Data \"<strong>" + formatTime(response.data.created_utc) + "</strong>\"</p>");
+					$("#result").html("<p>Data \"<strong>" + formatDateTime(response.data.created_utc) + "</strong>\"</p>");
 				}
 			});
 		}
@@ -23,23 +23,7 @@ $("#getRedditUNameForm").submit(function(){
 });
 
 
-function formatTime(unixTimestamp) {
+function formatDateTime(unixTimestamp) {
     var dt = new Date(unixTimestamp * 1000);
-
-    var hours = dt.getHours();
-    var minutes = dt.getMinutes();
-    var seconds = dt.getSeconds();
-
-    // the above dt.get...() functions return a single digit
-    // so I prepend the zero here when needed
-    if (hours < 10) 
-     hours = '0' + hours;
-
-    if (minutes < 10) 
-     minutes = '0' + minutes;
-
-    if (seconds < 10) 
-     seconds = '0' + seconds;
-
-    return hours + ":" + minutes + ":" + seconds;
+    return dt.toString();
 }
