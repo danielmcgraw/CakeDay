0ce86bd7361d02f6de4eeeb9c81369ec8c79f733
Daniel McGraw <dan.j.mcgraw@gmail.com>
2012-01-01 22:22:16 -0700

    Trim down js

--- js/script.js
+++ js/script.js
@@ -8,18 +8,14 @@ $("#getRedditUNameForm").submit(function(){
 		if (uname.length == 0) {
 			$("#uname").focus();
 		} else {
-			//$("#result").html(ajax_load);
 			alert("Get me some ajax")
-			$.getJSON(
+			$.ajax(
 				"http://www.reddit.com/user/" + uname + "/about.json",
-//				"q="+q,
-				{uname: uname},
 				function(json) {
 					alert("Callback!")
 					var result = "<p>Data \"<strong>" + json.responseData.data + "</strong>\"</p>";
 					$("#result").html(result);
-				}
-			);
+				});
 		}
-		return false;
-	});
+	return false;
+});
