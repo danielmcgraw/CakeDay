5febaa583e5dc7cabd9e007f34761247d6f13a00
Daniel McGraw <dan.j.mcgraw@gmail.com>
2012-01-02 15:19:26 -0700

    Clean up stragling code add div show UTC create date.

--- js/script.js
+++ js/script.js
@@ -2,31 +2,22 @@
 
 */
 
+$(document).ready(function() {
+	$("#uname").focus();
+});
+
 $("#getRedditUNameForm").submit(function(){
-	alert("Form Submit!");
 	var uname = $("#uname").val();
 		if (uname.length == 0) {
 			$("#uname").focus();
 		} else {
-			alert("Get me some ajax");
 			$.ajax({
 				url: "http://www.reddit.com/user/" + uname + "/about.json?jsonp=?",
 				dataType: "jsonp",
-				error: function(data) {
-					alert('DANGER DANGER WILL ROBINSON!');
-					console.log(data);
-				},
-				success: function(data) {
-					alert("Callback!");
-					console.log(data);
-					//var result = "<p>Data \"<strong>" + json + "</strong>\"</p>";
-					//$("#result").html(result);
+				success: function(response) {
+					$("#result").html("<p>Data \"<strong>" + response.data.created_utc + "</strong>\"</p>");
 				}
 			});
 		}
 	return false;
 });
-
-function hollaBack(json) {
-	console.logging(json);
-}
