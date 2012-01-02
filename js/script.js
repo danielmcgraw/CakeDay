4232a2064f815bfb52ababe8c2646be86ac4b159
Daniel McGraw <dan.j.mcgraw@gmail.com>
2012-01-02 15:05:01 -0700

    json type

--- js/script.js
+++ js/script.js
@@ -11,20 +11,14 @@ $("#getRedditUNameForm").submit(function(){
 			alert("Get me some ajax");
 			$.ajax({
 				url: "http://www.reddit.com/user/" + uname + "/about.json",
-				dataType: "script",
-				jsonp: false, 
-				jsonpCallback: "hollaBack",
-				error: function(data, textStatus, jqXHR) {
+				dataType: "json",
+				error: function(data) {
 					alert('DANGER DANGER WILL ROBINSON!');
 					console.log(data);
-					console.log(textStatus);
-					console.log(jqXHR);
 				},
-				success: function(data, textStatus, jqXHR) {
+				success: function(data) {
 					alert("Callback!");
 					console.log(data);
-					console.log(textStatus);
-					console.log(jqXHR);
 					//var result = "<p>Data \"<strong>" + json + "</strong>\"</p>";
 					//$("#result").html(result);
 				}
