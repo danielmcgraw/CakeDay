978e2357fbfcee2b7d26632d7df6e2150cd2c188
Daniel McGraw <dan.j.mcgraw@gmail.com>
2012-01-02 20:10:03 -0700

    Lets scale back

--- js/script.js
+++ js/script.js
@@ -4,30 +4,6 @@
 
 $(document).ready(function() {
 	$("#uname").focus();
-	
-	$("label.inlined + input.input-text").each(function (type) {
-		Event.observe(window, 'load', function () {
-			setTimeout(function(){
-				if (!input.value.empty()) {
-					input.previous().addClassName('has-text');
-				}
-			}, 200);
-		});
-
-		$(this).focus(function () {
-			$(this).prev("label.inlined").addClass("focus");
-		});
-
-		$(this).keypress(function () {
-			$(this).prev("label.inlined").addClass("has-text").removeClass("focus");
-		});
-
-		$(this).blur(function () {
-			if($(this).val() == "") {
-				$(this).prev("label.inlined").removeClass("has-text").removeClass("focus");
-			}
-		});
-	});
 });
 
 $("#getRedditCakeDayForm").submit(function(){
