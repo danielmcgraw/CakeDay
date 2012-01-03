9b4ce2855b11e69d2cb193455fb07e3ad32db904
Daniel McGraw <dan.j.mcgraw@gmail.com>
2012-01-03 12:30:28 -0700

    Css demo code

--- js/script.js
+++ js/script.js
@@ -4,6 +4,19 @@
 
 $(document).ready(function() {
 	$("#uname").focus();
+	$("label.inlined + .input-text").each(function (type) {
+		$(this).focus(function () {
+			$(this).prev("label.inlined").addClass("focus");
+		});
+		$(this).keypress(function () {
+			$(this).prev("label.inlined").addClass("has-text").removeClass("focus");
+		});
+		$(this).blur(function () {
+			if($(this).val() == "") {
+				$(this).prev("label.inlined").removeClass("has-text").removeClass("focus");
+			}
+		});
+	});
 });
 
 $("#getRedditCakeDayForm").submit(function(){
